0a038db9db7885b726887f541383d7562d7334d7
s4092979 <s4092979@student.rmit.edu.au>
2024-08-13 14:15:04 +1000

    js part1 & favicon test

---
+++ week05/Exercises/js02.js
@@ -0,0 +1,7 @@
+var string = "Luke Skywalker"
+var number = 34.7
+var boolean = true
+
+console.log(string)
+console.log(number)
+console.log(boolean)
---
+++ week05/Exercises/js03.js
@@ -0,0 +1,6 @@
+var valid1 = 1
+var valid2 = 2
+var valid3 = 3
+// var 1234 = 4
+// var if = 5
+// var 3invalid = 6
