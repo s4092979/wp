fb54c8f808a1ce32f33ef9dc37e627e92b6231a2
s4092979 <s4092979@student.rmit.edu.au>
2024-10-03 10:37:37 +1000

    dynamic table and gallery

--- a2/details.php
+++ a2/details.php
@@ -4,7 +4,7 @@ include "includes/header.inc";
 include "includes/nav.inc";
 include "includes/db_connect.inc";
 ?>
-    <main class="home">
+    <main>
         <img class="mainImg" src="images/main.jpg" alt="A dog and a cat">
         <h1 class="title">Pets Victoria</h1>
         <h2 class="subTitle">Welcome To Pet<br>Adoption</h2>
--- a2/gallery.php
+++ a2/gallery.php
@@ -1,92 +1,36 @@
 <?php
-    $title = "Pets Victoria";
-    include "includes/header.inc";
-    echo "\n";
-    include "includes/db_connect.inc";
-    echo "\n";
-    include "includes/nav.inc";
-    echo "\n";
+$title = "Pets Victoria";
+include "includes/header.inc";
+include "includes/nav.inc";
+include "includes/db_connect.inc";
 ?>
-    <main>
-        <p class="petsHeading">
-            Pets Victoria has a lot to offer
-        </p>
-        <p class="petsText">
-            For almost two decades, Pets Victoria has helped in creating true social change by bringing pet adoption into the mainstream. Our work has helped make a difference to the Victorian rescue community and thousands of pets in need of rescuse and rehabilitation. But, until every pet is safe, respected, and loved, we all still have big, hairy work to do.
-        </p>
-        <div class="galleryGrid">
-            <div class="box">
-                <div class="container">
-                    <img src="images/cat1.jpeg" alt="Milo">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Milo</p>
-            </div>
-            <div class="box">
-                <div class="container">
-                    <img src="images/dog1.jpeg" alt="Baxter">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Baxter</p>
-            </div>
-            <div class="box">
-                <div class="container">
-                    <img src="images/cat2.jpeg" alt="Luna">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Luna</p>
-            </div>
-            <div class="box">
-                <div class="container">
-                    <img src="images/dog2.jpeg" alt="Willow">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Willow</p>
-            </div>
-            <div class="box">
-                <div class="container">
-                    <img src="images/cat3.jpeg" alt="Oliver">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Oliver</p>
-            </div>
-            <div class="box">
-                <div class="container">
-                    <img src="images/dog3.jpeg" alt="Bella">
-                    <div class="overlay">
-                        <div class="text">
-                            <span class="material-symbols-outlined">search</span><br>
-                            <a>Discover more!</a>
-                        </div>
-                    </div>
-                </div>
-                <p>Bella</p>
-            </div>
-        </div>
-    </main>
+<main>
+    <p class="petsHeading">
+        Pets Victoria has a lot to offer
+    </p>
+    <p class="petsText">
+        For almost two decades, Pets Victoria has helped in creating true social change by bringing pet adoption into the mainstream. Our work has helped make a difference to the Victorian rescue community and thousands of pets in need of rescuse and rehabilitation. But, until every pet is safe, respected, and loved, we all still have big, hairy work to do.
+    </p>
+    <div class="galleryGrid">
+    <?php
+    $sql = "select * from pets";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            print "<div class='box'>\n";
+            print "<div class='container'>\n";
+            print "<img src='images/{$row['image']}' alt={$row['petname']}>\n";
+            print "<div class='overlay'>\n";
+            print "<div class='text'>\n";
+            print "<span class='material-symbols-outlined'>search</span><br>\n";
+            print "<a href=details.php?id={$row['petid']}>Discover more!</a>\n";
+            print "</div></div></div>\n";
+            print "<p>{$row['petname']}</p>\n";
+            print "</div>\n";
+        }
+    }
+    ?>
+    </div>
+</main>
 <?php include "includes/footer.inc"; ?>
