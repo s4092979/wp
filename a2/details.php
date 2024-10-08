943188d673aae49db6ec534c7413b0893f87ce4d
s4092979 <s4092979@student.rmit.edu.au>
2024-10-08 13:11:44 +1100

    link Pets and Gallery pages to Details

--- a2/details.php
+++ a2/details.php
@@ -3,12 +3,14 @@ $title = "Pets Victoria";
 include "includes/header.inc";
 include "includes/nav.inc";
 include "includes/db_connect.inc";
-?>
-    <main>
-        <img class="mainImg" src="images/main.jpg" alt="A dog and a cat">
-        <h1 class="title">Pets Victoria</h1>
-        <h2 class="subTitle">Welcome To Pet<br>Adoption</h2>
-    </main>
-<?php
+$id = $_GET['id'];
+$sql = "select * from pets where petid = $id";
+$result = $conn->query($sql);
+$row = mysqli_fetch_array($result);
+
+print "<main>";
+print "<img class='petsDetailsImg' src='images/{$row['image']}' alt={$row['petname']}>";
+
+
 include "includes/footer.inc";
 ?>
--- a2/pets.php
+++ a2/pets.php
@@ -22,42 +22,20 @@
                 <th>Age</th>
                 <th>Location</th>
             </tr>
-            <tr>
-                <td>Milo</td>
-                <td>Cat</td>
-                <td>3 months</td>
-                <td>Melbourne CBD</td>
-            </tr>
-            <tr>
-                <td>Baxter</td>
-                <td>Dog</td>
-                <td>5 months</td>
-                <td>Cape Woolamai</td>
-            </tr>
-            <tr>
-                <td>Luna</td>
-                <td>Cat</td>
-                <td>1 month</td>
-                <td>Ferntree Gully</td>
-            </tr>
-            <tr>
-                <td>Willow</td>
-                <td>Dog</td>
-                <td>48 months</td>
-                <td>Marysville</td>
-            </tr>
-            <tr>
-                <td>Oliver</td>
-                <td>Cat</td>
-                <td>12 months</td>
-                <td>Grampians</td>
-            </tr>
-            <tr>
-                <td>Bella</td>
-                <td>Dog</td>
-                <td>10 months</td>
-                <td>Carlton</td>
-            </tr>
+            <?php
+            $sql = "select * from pets";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    print "<tr>\n";
+                    print "<td><a href=details.php?id={$row['petid']}>{$row['petname']}</a></td>\n";
+                    print "<td>{$row['type']}</td>\n";
+                    print "<td>{$row['age']}</td>\n";
+                    print "<td>{$row['location']}</td>\n";
+                    print "</tr>\n";
+                }
+            }
+            ?>
         </table>
     </main>
 <?php include "includes/footer.inc"; ?>
